e772ca2fecf99d3a2bd1d14716c5e26e43af548d
com.Kondratov <com.Kondratov@gmail.com>
2020-10-06 20:50:56 +0300

    Добавьте файлы проекта.

---
+++ TelegramBot(CORE)/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TelegramBot_CORE_.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
