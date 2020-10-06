052701cc4de67408ae38e2db77c52dfac5afddf0
com.Kondratov <com.Kondratov@gmail.com>
2020-10-06 21:03:15 +0300

    Created a new TelegramBot in a telegram, Removed all unnecessary from the project.

--- TelegramBot(CORE)/Controllers/HomeController.cs
+++ TelegramBot(CORE)/Controllers/HomeController.cs
@@ -6,32 +6,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using TelegramBot_CORE_.Models;
+using Telegram.Bot;
 
 namespace TelegramBot_CORE_.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private TelegramBotClient _teleBot;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger,TelegramBotClient telegram)
         {
             _logger = logger;
+            _teleBot = telegram;
         }
 
         public IActionResult Index()
         {
+            _teleBot = new TelegramBotClient("1255618515:AAEclZ8vcKhGW45T2MOwjnYF-3NmN6LSqAg");
+
+            ViewData["NameOfBot"] = _teleBot.GetMeAsync().Result; // ПОЛУЧАЕМ ИМЯ БОТА
+
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
